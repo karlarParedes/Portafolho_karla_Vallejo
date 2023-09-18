71db89e8d4327f1b8a43472361829c0a97c39be2
karla Paredes.github.io <115048439+karlarParedes@users.noreply.github.com>
2023-09-18 00:47:10 -0300

    Uploading project: Header toggle made with JS mobile dropdown menu

--- script.js/script.js
+++ script.js/script.js
@@ -1,16 +1,16 @@
 // Header toggle
 
-let MenuBtb = document.getElementById('MenuBtn')
+let MenuBtn = document.getElementById('MenuBtn')
 
 MenuBtn.addEventListener('click', function(e) {
     document.querySelector('body').classList.toggle('mobile-nav-active');
-    this.classLis.ttoggle('fa-xmark')
+    this.classList.toggle('fa-xmark')
 })
 
 // Typing Effect
-let Typing = new Typed('auto-input', {
-    Strings: ['Front-End Developer!', 'Front-End Developer!', 'Front-End Developer!', 'Fron-End Developer!'],
-    typeSeed: 100,
+let typed = new Typed('.auto-input', {
+    strings: ['Sou', 'Front-End', 'Sou Front-End Developer!'],
+    typeSpeed: 100,
     backSpeed: 100,
     backDelay: 2000,
     loop: true,
